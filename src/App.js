787129094da3afc1b782e350be514de085c4f72d
Andre Felipe <afelipes@gmail.com>
2024-02-16 20:17:04 -0300

    feat: implemented the function of manipulating the part position randomly by setInterval

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Board from './components/Board';
+import { observe } from './services/Game'
+
 
 function App() {
+  const [knightPosition, setKnightPosition] = useState([4, 5]);
+
+  useEffect(() => {
+    observe(newKnightPosition => {
+      setKnightPosition(newKnightPosition);
+    });
+
+  }, []);  // Executar o efeito apenas uma vez na montagem do componente
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Board knightPosition={knightPosition} />
     </div>
   );
 }
---
+++ src/components/Knight.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Knight() {
+  return (
+    <div style={{
+      display: 'flex', 
+      width: '100%', 
+      height: '100%', 
+      fontSize: '300%',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>♘</div>
+  )
+}
